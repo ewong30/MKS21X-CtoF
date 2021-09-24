805d779643c6d29558c68bcda7dad1e57c3b34bb
Ethan Wong <ewong30@stuy.edu>
2021-09-23 23:04:00 -0400

    Finished and added a main to call the functions, added test values too

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,23 @@
 public class CtoFTester {
     public static double celsiusToFarenheit(double cDegrees) {
-        return (cDegrees * (9/5)) + 32;
+        return (cDegrees * (9.0 / 5)) + 32;
     }
 
     public static double farenheitToCelsius(double fDegrees) {
-        return ((fDegrees - 32) * (5/9));
+        return (fDegrees - 32) * (5.0 / 9);
+    }
+
+    public static void main(String[]args) {
+        //Test Cases for C to F conversion:
+        System.out.println("The values for C to F Conversion(test):");
+        System.out.println(celsiusToFarenheit(0));
+        System.out.println(celsiusToFarenheit(25));
+        System.out.println(celsiusToFarenheit(100));
+
+        //Test Cases for F to C conversion:
+        System.out.println("The values for F to C Conversion(Test):");
+        System.out.println(farenheitToCelsius(32));
+        System.out.println(farenheitToCelsius(75));
+        System.out.println(farenheitToCelsius(212));
     }
 }
