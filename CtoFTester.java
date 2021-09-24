dfb7c7252e8c30ab2bd75e89d67d142e98620886
Ethan Wong <ewong30@stuy.edu>
2021-09-23 22:56:27 -0400

    Added an F to C conversion this time

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,4 +2,8 @@ public class CtoFTester {
     public static double celsiusToFarenheit(double cDegrees) {
         return (cDegrees * (9/5)) + 32;
     }
+
+    public static double farenheitToCelsius(double fDegrees) {
+        return ((fDegrees - 32) * (5/9));
+    }
 }
